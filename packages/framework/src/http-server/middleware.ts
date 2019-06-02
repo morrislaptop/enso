15e578b6ae0261d7620ea67914520ffaabfb825b
1a35e1 <1a35e1@protonmail.com>
2019-06-02 14:26:14 +1000

    Added `koa-multer` to recommended middleware

--- packages/framework/src/http-server/middleware.ts
+++ packages/framework/src/http-server/middleware.ts
@@ -2,12 +2,26 @@ import morgan from 'koa-morgan'
 import helmet from 'koa-helmet'
 import jwt from 'koa-jwt'
 import cors from '@koa/cors'
+/**
+ * Request parsing
+ *
+ * https://www.npmtrends.com/koa-bodyparser-vs-koa-body-vs-koa-multer-vs-co-busboy
+ *
+ * koa-bodyparser [json], [form] and [text]
+ *  => ctx.req.body
+ *
+ * koa-multer [multipart]
+ *  => ctx.req.files | ctx.req.file
+ */
+import multer from 'koa-multer'
 import bodyparser from 'koa-bodyparser'
+
 
 export {
   morgan,
   helmet,
   jwt,
   cors,
-  bodyparser
+  bodyparser,
+  multer
 }
